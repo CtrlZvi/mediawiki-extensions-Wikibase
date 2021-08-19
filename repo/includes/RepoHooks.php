3127951a0a1a23fb78fc8b20167a648bf9c0ac3d
Zvi "CtrlZvi" Effron <viz_skywalker+GitHub@outlook.com>
2021-08-18 19:18:09 -0700

    [WIP] AfterImportPage hook

--- repo/includes/RepoHooks.php
+++ repo/includes/RepoHooks.php
@@ -942,6 +942,20 @@ final class RepoHooks {
 	}
 
 	/**
+	 * Called by Import.php. Implemented to update the entity id counters.
+	 *
+	 * @param Title $title Title under which the revisions were imported
+	 * @param ForeignTitle $foreignTitle ForeignTitle object based on data provided by the XML file
+	 * @param int $revCount Number of revisions in the XML file
+	 * @param int $sRevCount Number of successfully imported revisions
+	 * @param array $pageInfo Associative array of page information
+	 * @return bool|void True or no return value to continue or false to abort
+	 */
+	public static function onAfterImportPage( $importer, $pageInfo, $revisionInfo ) {
+		return;
+	}
+
+	/**
 	 * Add Concept URI link to the toolbox section of the sidebar.
 	 *
 	 * @param Skin $skin
