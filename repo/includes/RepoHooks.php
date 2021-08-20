7cb93b4b4dbf9dafe3ce95abd7009424b23a0316
Zvi "CtrlZvi" Effron <viz_skywalker+WikimediaDeveloperAccount@outlook.com>
2021-08-19 23:59:06 -0700

    [WIP] Replace the AfterImportPage hook with modifications to ImportHandleRevisionXMLTag
    Change-Id: Ia26368479f16bffc8008aeacee7417de8f8c1a91

--- repo/includes/RepoHooks.php
+++ repo/includes/RepoHooks.php
@@ -931,7 +931,11 @@ final class RepoHooks {
 			$contentModels = WikibaseRepo::getContentModelMappings();
 			$allowImport = WikibaseRepo::getSettings()->getSetting( 'allowEntityImport' );
 
-			if ( !$allowImport && in_array( $revisionInfo['model'], $contentModels ) ) {
+			if ( !in_array( $revisionInfo['model'], $contentModels ) ) {
+				return;
+			}
+
+			if ( !$allowImport ) {
 				// Skip entities.
 				// XXX: This is rather rough.
 				throw new MWException(
@@ -939,70 +943,49 @@ final class RepoHooks {
 						. ' You can enable imports using the "allowEntityImport" setting.'
 				);
 			}
-		}
-	}
 
-		/**
-		 * This hook is called when a page import is completed.
-		 *
-		 * @since 1.35
-		 *
-		 * @param Title $title Title under which the revisions were imported
-		 * @param ForeignTitle $foreignTitle ForeignTitle object based on data provided by the XML file
-		 * @param int $revCount Number of revisions in the XML file
-		 * @param int $sRevCount Number of successfully imported revisions
-		 * @param array $pageInfo Associative array of page information
-		 * @return bool|void True or no return value to continue or false to abort
-		 */
-	public function onAfterImportPage(
-		$title,
-		$foreignTitle,
-		$revCount,
-		$sRevCount,
-		$pageInfo
-	) {
-		$contentModels = WikibaseRepo::getContentModelMappings();
-		$pageContentModel = $title->getContentModel();
-		if ( !in_array( $pageContentModel, $contentModels ) ) {
-			return;
-		}
+			if (!isset($pageInfo['_title'])) {
+				return;
+			}
 
-		$highestId = MediaWikiServices::getInstance()
-			->getDBLoadBalancer()
-			->getConnection( DB_REPLICA )
-			->selectRow(
-				'wb_id_counters',
-				'id_value',
-				[ 'id_type' => $pageContentModel ],
-				__METHOD__
-			);
-
-		if ( !$highestId ) {
-			$storedId = 0;
-		} else {
-			$storedId = (int)$highestId->id_value;
-		}
-
-		$entityIdLookup = WikibaseRepo::getEntityIdLookup();
-		$id = $entityIdLookup->getEntityIdForTitle( $title );
-		if ( !$id instanceof Int32EntityId ) {
-			return;
-		}
-		$newId = $id->getNumericId();
-		if ( $newId > $storedId ) {
-			MediaWikiServices::getInstance()
+			$entityIdLookup = WikibaseRepo::getEntityIdLookup();
+			$id = $entityIdLookup->getEntityIdForTitle( $pageInfo['_title'] );
+			if ( !$id instanceof Int32EntityId ) {
+				return;
+			}
+			$newId = $id->getNumericId();
+		
+			$highestId = MediaWikiServices::getInstance()
 				->getDBLoadBalancer()
-				->getConnection( DB_PRIMARY )
-				->upsert(
+				->getConnection( DB_REPLICA )
+				->selectRow(
 					'wb_id_counters',
-					[
-						'id_type' => $pageContentModel,
-						'id_value' => $newId,
-					],
-					'id_type',
-					[ "id_value = $newId" ],
+					'id_value',
+					[ 'id_type' => $revisionInfo['model'] ],
 					__METHOD__
 				);
+
+			if ( !$highestId ) {
+				$storedId = 0;
+			} else {
+				$storedId = (int)$highestId->id_value;
+			}
+
+			if ( $newId > $storedId ) {
+				MediaWikiServices::getInstance()
+					->getDBLoadBalancer()
+					->getConnection( DB_PRIMARY )
+					->upsert(
+						'wb_id_counters',
+						[
+							'id_type' => $revisionInfo['model'],
+							'id_value' => $newId,
+						],
+						'id_type',
+						[ 'id_value' => $newId ],
+						__METHOD__
+					);
+			}
 		}
 	}
 
